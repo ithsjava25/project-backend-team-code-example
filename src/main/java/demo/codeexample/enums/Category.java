55e6a6409492969e5bbd1d0ce4a943538ce050e0
Anna Ziafar <annaaziafar@gmail.com>
2026-03-28 13:23:24 +0100

    Remove displayname from enums in order to seperate concerns and keep enum clean

--- src/main/java/demo/codeexample/enums/Category.java
+++ src/main/java/demo/codeexample/enums/Category.java
@@ -1,18 +1,6 @@
 package demo.codeexample.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum Category {
-    FILM("Film"),
-    SERIES("Series");
-
-    private final String displayName;
-
-    @Override
-    public String toString(){
-        return displayName;
-    }
+    FILM,
+    SERIES;
 }
--- src/main/java/demo/codeexample/enums/Genre.java
+++ src/main/java/demo/codeexample/enums/Genre.java
@@ -1,24 +1,12 @@
 package demo.codeexample.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum Genre {
-    ACTION("Action"),
-    COMEDY("Comedy"),
-    CRIME("Crime"),
-    DRAMA("Drama"),
-    FANTASY("Fantasy"),
-    HORROR("Horror"),
-    ROMANCE("Romance"),
-    SCI_FI("Sci-Fi");
-
-    private final String displayName;
-
-    @Override
-    public String toString(){
-        return displayName;
-    }
+    ACTION,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    ROMANCE,
+    SCI_FI
 }
