73713c01c04c0e032e61c60c55f42e2d994ed409
Anna Ziafar <annaaziafar@gmail.com>
2026-03-28 12:37:22 +0100

    Start coding entity, not done

--- src/main/java/demo/codeexample/project/ProjectEntity.java
+++ src/main/java/demo/codeexample/project/ProjectEntity.java
@@ -1,4 +1,26 @@
 package demo.codeexample.project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    Long id;
+
+    String title;
+
+
 }
