ec133109b91d8e898b6a6ba70cd68ca0759f6a37
Eric Phu <eraiicphu@gmail.com>
2026-03-29 14:53:08 +0200

    Created TaskEntity and TaskRepository

--- src/main/java/demo/codeexample/task/TaskEntity.java
+++ src/main/java/demo/codeexample/task/TaskEntity.java
@@ -1,4 +1,41 @@
 package demo.codeexample.task;
 
+import demo.codeexample.employee.EmployeeEntity;
+import demo.codeexample.project.ProjectEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column
+    private String Title;
+    @Column
+    private String Description;
+    @Column
+    private String Status;
+    @Column
+    private LocalDateTime Deadline;
+
+    @JoinColumn
+    @ManyToOne
+    private ProjectEntity Project;
+
+    @JoinColumn
+    @ManyToOne
+    private EmployeeEntity Employee;
+
+
+
+
 }
