6d5b0571370c758c4f2e7b5774ddadea2c09b8bf
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-29 20:06:30 +0200

    Create CommentEntity

--- src/main/java/demo/codeexample/comment/CommentEntity.java
+++ src/main/java/demo/codeexample/comment/CommentEntity.java
@@ -1,15 +1,44 @@
 package demo.codeexample.comment;
 
-import jakarta.persistence.Entity;
+import demo.codeexample.task.TaskEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
 public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String content;
+
+    @Column(nullable = false)
+    private Long writerId;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private TaskEntity task;
+
+    // Ska vi ha en tid och datum när kommentaren skrevs?
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 
 }
