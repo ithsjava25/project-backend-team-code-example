090b396cfb206b2f302e782116c63dff1858929a
Anna Ziafar <annaaziafar@gmail.com>
2026-03-30 11:03:15 +0200

    Refactor

--- src/main/java/demo/codeexample/project/CreateProjectDto.java
+++ src/main/java/demo/codeexample/project/CreateProjectDto.java
@@ -2,20 +2,21 @@ package demo.codeexample.project;
 
 import demo.codeexample.enums.Category;
 import demo.codeexample.enums.Genre;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CreateProjectDto {
     @NotNull(message = "You must choose a title for the project")
     String title;
 
+    @Future(message = "Movie can't be released in past")
     LocalDate releaseDate;
 
     @NotNull(message = "You must choose a category")
--- src/main/java/demo/codeexample/project/ProjectDto.java
+++ src/main/java/demo/codeexample/project/ProjectDto.java
@@ -2,18 +2,11 @@ package demo.codeexample.project;
 
 import demo.codeexample.enums.Category;
 import demo.codeexample.enums.Genre;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ProjectDto {
     Long id;
     String title;
