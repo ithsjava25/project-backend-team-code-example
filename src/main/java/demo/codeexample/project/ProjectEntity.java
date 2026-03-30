1c80f750c8199185ca07ffcb65292f4163d74330
Anna Ziafar <annaaziafar@gmail.com>
2026-03-30 11:03:15 +0200

    Refactor

--- src/main/java/demo/codeexample/project/ProjectEntity.java
+++ src/main/java/demo/codeexample/project/ProjectEntity.java
@@ -5,8 +5,10 @@ import demo.codeexample.enums.Genre;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -40,4 +42,19 @@ public class ProjectEntity {
         this.imageURL = imageURL;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ProjectEntity that = (ProjectEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
