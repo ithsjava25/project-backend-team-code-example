ac75ecae5133d42a4ebc3ecd4ef2aa5d9646ce9c
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-30 10:55:00 +0200

    update CommentEntity - equals, hashcode
    Create CommentRepository
    
    CreateCommentDto, and CommentDto

--- src/main/java/demo/codeexample/comment/CommentEntity.java
+++ src/main/java/demo/codeexample/comment/CommentEntity.java
@@ -6,8 +6,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -40,5 +42,26 @@ public class CommentEntity {
         this.createdAt = LocalDateTime.now();
     }
 
+    public  CommentEntity(String content, Long writerId,TaskEntity task, LocalDateTime createdAt) {
+        this.content = content;
+        this.writerId = writerId;
+        this.task = task;
+        this.createdAt = createdAt;
+    }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        CommentEntity that = (CommentEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
