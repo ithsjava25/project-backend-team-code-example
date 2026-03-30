2cd4256d5b2b2feffd21ba84d4dfcd9deba47d1a
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-03-30 10:55:00 +0200

    update CommentEntity - equals, hashcode
    Create CommentRepository
    
    CreateCommentDto, and CommentDto

---
+++ src/main/java/demo/codeexample/comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package demo.codeexample.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+}
