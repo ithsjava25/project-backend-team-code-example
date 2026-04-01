ddfd6d5fc07e7e6d6ff3632cca22047f63356b54
Eric Phu <eraiicphu@gmail.com>
2026-04-01 23:56:06 +0200

    Add Task domain, implemented TaskUseCase for IN-Data implementeed TaskRepositoryPort

---
+++ src/main/java/demo/codeexample/task/application/ports/out/TaskRepositoryPort.java
@@ -0,0 +1,15 @@
+package demo.codeexample.task.application.ports.out;
+
+import demo.codeexample.task.TaskEntity;
+import demo.codeexample.task.domain.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepositoryPort {
+    List<Task> findAll();
+
+    Task save(Task task);
+
+    Optional<Task> findById(Long id);
+}
---
+++ src/main/java/demo/codeexample/task/domain/Task.java
@@ -0,0 +1,57 @@
+package demo.codeexample.task.domain;
+
+import demo.codeexample.enums.TaskStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private final Long id ;
+    private final String title ;
+    private final String description ;
+    private final TaskStatus status ;
+    private final LocalDateTime deadline ;
+    private final Long projectId ;
+    private final Long userId ;
+
+
+    public Task(Long id, String title, String description, TaskStatus status, LocalDateTime deadline, Long projectId, Long userId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.deadline = deadline;
+        this.projectId = projectId;
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+}
