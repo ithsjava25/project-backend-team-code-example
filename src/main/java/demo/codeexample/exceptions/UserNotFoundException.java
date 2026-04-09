995502aed37119d586379bf5578dc4d65019d0cf
Stelle83 <stelle83@gmail.com>
2026-04-09 23:28:19 +0200

    update- userNotFoundException

--- src/main/java/demo/codeexample/exceptions/UserNotFoundException.java
+++ src/main/java/demo/codeexample/exceptions/UserNotFoundException.java
@@ -1,6 +1,12 @@
 package demo.codeexample.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Long id) {
+        super("User not found with id: " + id);
+    }
+
+    // Keep for flexibility
     public UserNotFoundException(String message) {
         super(message);
     }
