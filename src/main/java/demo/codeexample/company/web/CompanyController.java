b22c53321bfd0cb4604bfce3be0eda836d97a633
Fredrik Mohlen <fredrikmohlen@gmail.com>
2026-04-10 15:59:42 +0200

    add different styles and views for homepage, will remove the ones which we don't approve

--- src/main/java/demo/codeexample/company/web/CompanyController.java
+++ src/main/java/demo/codeexample/company/web/CompanyController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @AllArgsConstructor
 @RequestMapping
@@ -23,8 +24,15 @@ public class CompanyController {
     }
 
     @GetMapping("home2")
-    public String home(Model model) {
-        model.addAttribute("companyName", "Hexagonal Film Productions");
+    public String home2() {
         return "home2";
+    }
+    @GetMapping("home3")
+    public String home3() {
+        return "home3";
+    }
+    @GetMapping("home4")
+    public String home4() {
+        return "home4";
     }
 }
