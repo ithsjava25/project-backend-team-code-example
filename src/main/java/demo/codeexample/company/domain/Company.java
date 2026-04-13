2cd01f8c28f333e4f37128fdd4e31cedf2cdcdd3
Stelle83 <stelle83@gmail.com>
2026-04-13 17:53:50 +0200

    update Company

--- src/main/java/demo/codeexample/company/domain/Company.java
+++ src/main/java/demo/codeexample/company/domain/Company.java
@@ -10,7 +10,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.proxy.HibernateProxy;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -32,12 +34,17 @@ public class Company {
     @Email
     private String email;
 
-    //Todo: Add List With Employees
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "company_projects", joinColumns = @JoinColumn(name = "company_id"))
+    @Column(name = "project_id")
+    Set<Long> projectsId = new HashSet<>();
 
-    public Company(String companyName, Address address, String email){
+
+    public Company(String companyName, Address address, String email, Set<Long> projectsId){
         this.companyName = companyName;
         this.address = address;
         this.email = email;
+        this.projectsId = projectsId;
     }
 
     @Override
