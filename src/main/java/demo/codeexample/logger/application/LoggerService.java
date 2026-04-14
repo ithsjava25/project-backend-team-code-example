4bdd873f4480310a75300a712f8b92fc4527f72d
Eric Phu <eraiicphu@gmail.com>
2026-04-14 21:24:15 +0200

    Commit after rebase Logger implements LoggerLookup (LoggerPort)

--- src/main/java/demo/codeexample/logger/application/LoggerService.java
+++ src/main/java/demo/codeexample/logger/application/LoggerService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Primary
-public class LoggerService implements LoggerPort{
+public class LoggerService implements LoggerLookup{
 
     private final LoggerRepository loggerRepository;
 
