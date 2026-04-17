954ae9a1a8d94be9ae8c9c3731cf229340a10251
mikee13d <229708831+mikee13d@users.noreply.github.com>
2026-04-17 13:40:03 +0200

    producers dashboard and create project pages

---
+++ src/main/java/demo/codeexample/project/CreateProjectDto.java
@@ -0,0 +1,21 @@
+package demo.codeexample.project;
+
+import demo.codeexample.project.domain.Category;
+import demo.codeexample.project.domain.Genre;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public record CreateProjectDto(
+        String title,
+        String description,
+        LocalDate releaseDate,
+        Set<Long> employeesId,
+        Category category,
+        Genre genre,
+        Long fileId,
+        Long companyId
+) {}
+
+
+
