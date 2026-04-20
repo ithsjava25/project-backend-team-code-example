ed8093abcb9faa65d7adfccb2dde9b6839ca6656
Eric Phu <eraiicphu@gmail.com>
2026-04-21 00:19:40 +0200

    added classes for Event driven logic

---
+++ src/main/java/demo/codeexample/task/infrastructure/adapters/in/ProjectEventListenerAdapter.java
@@ -0,0 +1,22 @@
+package demo.codeexample.task.infrastructure.adapters.in;
+
+import demo.codeexample.project.ProjectCreatedEvent;
+import demo.codeexample.task.application.ports.in.TaskUseCase;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProjectEventListenerAdapter {
+
+    private final TaskUseCase taskUseCase;
+
+    @EventListener
+    public void handle(ProjectCreatedEvent event) {
+        System.out.println("Event mottaget för projekt: " + event.title());//debuug
+        taskUseCase.handleProjectCreated(event);
+        }
+    }
