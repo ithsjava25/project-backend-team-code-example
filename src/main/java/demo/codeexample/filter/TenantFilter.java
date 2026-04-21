d8b53a0a456a378bfb37ef87cb0d2b25807d828a
Stelle83 <stelle83@gmail.com>
2026-04-21 16:40:58 +0200

    update TenantFilter

--- src/main/java/demo/codeexample/filter/TenantFilter.java
+++ src/main/java/demo/codeexample/filter/TenantFilter.java
@@ -9,18 +9,46 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class TenantFilter implements HandlerInterceptor {
 
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,
+//                             HttpServletResponse response,
+//                             Object handler) {
+//
+//        String[] parts = request.getRequestURI().split("/");
+//        if (parts.length > 1) {
+//            TenantContext.setTenant(parts[1]);
+//            //Validate company name?
+//            //Get more info from db?
+//        } else
+//            TenantContext.setTenant("");
+//        return true;
+//    }
+
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) {
 
-        String[] parts = request.getRequestURI().split("/");
+        String uri = request.getRequestURI();
+        String[] parts = uri.split("/");
+
         if (parts.length > 1) {
-            TenantContext.setTenant(parts[1]);
-            //Validate company name?
-            //Get more info from db?
-        } else
+            String firstSegment = parts[1];
+
+            if (!firstSegment.equals("css")
+                    && !firstSegment.equals("js")
+                    && !firstSegment.equals("images")
+                    && !firstSegment.equals("oauth2")
+                    && !firstSegment.equals("login")
+                    && !firstSegment.equals("web")) {
+                TenantContext.setTenant(firstSegment);
+            } else {
+                TenantContext.setTenant("");
+            }
+        } else {
             TenantContext.setTenant("");
+        }
+
         return true;
     }
 
