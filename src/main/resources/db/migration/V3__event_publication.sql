4d4eee5763801632c632d88e93404b43b8330e12
Eric Phu <eraiicphu@gmail.com>
2026-04-21 14:34:39 +0200

    CodeRabbit changes new flyway V3__

--- src/main/resources/db/migration/V3__event_publication.sql
+++ src/main/resources/db/migration/V3__event_publication.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS EVENT_PUBLICATION (
-                                                 ID               BINARY(16) NOT NULL,
-    COMPLETION_DATE  DATETIME(6),
-    EVENT_TYPE       VARCHAR(512) NOT NULL,
-    LISTENER_ID      VARCHAR(512) NOT NULL,
-    PUBLICATION_DATE DATETIME(6) NOT NULL,
-    SERIALIZED_EVENT TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS event_publication (
+    id               BINARY(16) NOT NULL,
+    completion_date  DATETIME(6),
+    event_type       VARCHAR(512) NOT NULL,
+    listener_id      VARCHAR(512) NOT NULL,
+    publication_date DATETIME(6) NOT NULL,
+    serialized_event TEXT NOT NULL,
     PRIMARY KEY (ID)
     ) ENGINE=InnoDB;
 
 
 CREATE INDEX event_publication_by_completion_date_idx
-    ON EVENT_PUBLICATION (COMPLETION_DATE);
+    ON event_publication (completion_date);
