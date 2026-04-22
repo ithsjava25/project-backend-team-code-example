ba697b39fa60e43d2f86c57fb6248e91ec35a424
Stelle83 <stelle83@gmail.com>
2026-04-22 14:58:47 +0200

    create CompanyLookup, update CompnayRepository

--- src/main/java/demo/codeexample/company/CompanyLookup.java
+++ src/main/java/demo/codeexample/company/CompanyLookup.java
@@ -1,5 +1,14 @@
 package demo.codeexample.company;
 
+import java.util.Optional;
+
 public interface CompanyLookup {
     Long getCompanyIdFromName(String companyName);
+
+//    Optional<CompanyDto> findById(Long id);
+//
+//    Optional<CompanyDto> findByTenant(String tenant);
+//
+//    record CompanyDto(Long id, String companyName, String tenant) {}
+
 }
