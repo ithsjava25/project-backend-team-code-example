554687fb554eac841384c158fa06c983465a570f
Stelle83 <stelle83@gmail.com>
2026-04-26 21:15:40 +0200

    Restrict NoCacheFilter to sensitive application routes

---
+++ src/main/java/demo/codeexample/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package demo.codeexample.config;
+
+import demo.codeexample.security.NoCacheFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<NoCacheFilter> noCacheFilter() {
+        FilterRegistrationBean<NoCacheFilter> registration = new FilterRegistrationBean<>();
+
+        registration.setFilter(new NoCacheFilter());
+
+        registration.addUrlPatterns(
+                "/dashboard/*",
+                "/projects/*",
+                "/login/*",
+                "/logout"
+        );
+
+        registration.setOrder(1);
+
+        return registration;
+    }
+}
